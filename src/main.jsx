18d0ba6cea84250a6822afdd47ddf19cf33da0b0
RAFiSGODOY <rafaelgpinguelo@gmail.com>
2024-10-22 11:34:21 -0300

    Inicio do Projeto

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import Login from "./pages/login/login.jsx";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+   <Login />
+  
+)
---
+++ src/pages/appointments/appointments.jsx
@@ -0,0 +1,8 @@
+function Appointments(){
+
+    return <>
+    <h1>Agendamentos</h1>
+    </>
+}
+
+export default Appointments;
